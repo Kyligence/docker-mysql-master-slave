4129ef5cf8c9a3b3b721bbb7911f4667f6a29b34
Hao Cheng <hao.cheng@kyligence.io>
2019-12-30 11:18:54 +0800

    load balance mysql

--- build.sh
+++ build.sh
@@ -1,38 +1,53 @@
 #!/bin/bash
 
 docker-compose down
-rm -rf ./master/data/*
-rm -rf ./slave/data/*
+rm -rf ./mysql_master_1/data/*
+rm -rf ./mysql_master_2/data/*
 docker-compose build
 docker-compose up -d
 
-until docker exec mysql_master sh -c 'export MYSQL_PWD=111; mysql -u root -e ";"'
+until docker exec mysql_master_1 sh -c 'export MYSQL_PWD=111; mysql -u root -e ";"'
 do
     echo "Waiting for mysql_master database connection..."
     sleep 4
 done
 
-priv_stmt='GRANT REPLICATION SLAVE ON *.* TO "mydb_slave_user"@"%" IDENTIFIED BY "mydb_slave_pwd"; FLUSH PRIVILEGES;'
-docker exec mysql_master sh -c "export MYSQL_PWD=111; mysql -u root -e '$priv_stmt'"
+priv_stmt='GRANT REPLICATION SLAVE ON *.* TO "replicauser"@"%" IDENTIFIED BY "111"; FLUSH PRIVILEGES;'
+docker exec mysql_master_1 sh -c "export MYSQL_PWD=111; mysql -u root -e '$priv_stmt'"
 
-until docker-compose exec mysql_slave sh -c 'export MYSQL_PWD=111; mysql -u root -e ";"'
+until docker-compose exec mysql_master_2 sh -c 'export MYSQL_PWD=111; mysql -u root -e ";"'
 do
     echo "Waiting for mysql_slave database connection..."
     sleep 4
 done
+docker exec mysql_master_2 sh -c "export MYSQL_PWD=111; mysql -u root -e '$priv_stmt'"
 
 docker-ip() {
     docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$@"
 }
 
-MS_STATUS=`docker exec mysql_master sh -c 'export MYSQL_PWD=111; mysql -u root -e "SHOW MASTER STATUS"'`
-CURRENT_LOG=`echo $MS_STATUS | awk '{print $6}'`
-CURRENT_POS=`echo $MS_STATUS | awk '{print $7}'`
+stmt() {
+    MS_STATUS=`docker exec $1 sh -c 'export MYSQL_PWD=111; mysql -u root -e "SHOW MASTER STATUS"'`
+    CURRENT_LOG=`echo $MS_STATUS | awk '{print $6}'`
+    CURRENT_POS=`echo $MS_STATUS | awk '{print $7}'`
 
-start_slave_stmt="CHANGE MASTER TO MASTER_HOST='$(docker-ip mysql_master)',MASTER_USER='mydb_slave_user',MASTER_PASSWORD='mydb_slave_pwd',MASTER_LOG_FILE='$CURRENT_LOG',MASTER_LOG_POS=$CURRENT_POS; START SLAVE;"
-start_slave_cmd='export MYSQL_PWD=111; mysql -u root -e "'
-start_slave_cmd+="$start_slave_stmt"
-start_slave_cmd+='"'
-docker exec mysql_slave sh -c "$start_slave_cmd"
+    echo "STOP SLAVE;CHANGE MASTER TO MASTER_HOST='$(docker-ip $1)',MASTER_USER='replicauser',MASTER_PASSWORD='111',MASTER_LOG_FILE='$CURRENT_LOG',MASTER_LOG_POS=$CURRENT_POS; START SLAVE;" 
+}
+start_master_2_cmd='export MYSQL_PWD=111; mysql -u root -e "'
+start_master_2_cmd+="$(stmt mysql_master_1)"
+start_master_2_cmd+='"'
+start_master_1_cmd='export MYSQL_PWD=111; mysql -u root -e "'
+start_master_1_cmd+="$(stmt mysql_master_2)"
+start_master_1_cmd+='"'
+docker exec mysql_master_2 sh -c "$start_master_2_cmd"
+docker exec mysql_master_2 sh -c "export MYSQL_PWD=111; mysql -u root -e 'SHOW SLAVE STATUS \G'"
 
-docker exec mysql_slave sh -c "export MYSQL_PWD=111; mysql -u root -e 'SHOW SLAVE STATUS \G'"
+docker exec mysql_master_1 sh -c "$start_master_1_cmd"
+docker exec mysql_master_1 sh -c "export MYSQL_PWD=111; mysql -u root -e 'SHOW SLAVE STATUS \G'"
+
+
+create_ha_user='export MYSQL_PWD=111; mysql -u root -e "'
+create_ha_user+="CREATE USER 'haproxy_check'@'%';FLUSH PRIVILEGES;"
+create_ha_user+='"'
+docker exec mysql_master_1 sh -c "$create_ha_user"
+
